9e5c91c8e372eec1dc1d5a8943ffe76a540c1c20
jsizemo <jason.sizemore@e-farmcredit.com>
2022-07-07 18:26:36 -0400

    First Commit

---
+++ Kanye-Rest/Kanye-Rest/Pages/Response.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kanye_Rest.Pages
+{
+    public class ResponseModel : PageModel
+    {
+        public string ResponseBody { get; set; }
+
+        public void OnGet(string result)
+        {
+            ResponseBody = result;
+        }
+    }
+}
